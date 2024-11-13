cbe595cef6b22dbd1826a3e4016539905a0d1588
guivascs <guilhermemicroban@gmail.com>
2024-11-13 10:53:57 -0300

    Formatando a tabuada para uma melhor visualização

--- tabuada.py
+++ tabuada.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-__version__ = "0.0.1" 
+__version__ = "0.1.1" 
 
 
 # base = [1,2,3,4,5,6,7,8,9,10]
 numeros = list(range(1,11))
 
 #Itarable (precorrivel)
-for numero in numeros: 
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print ("-----------------")
+for n1 in numeros:
+    print()
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    bloco = ""
+    for n2 in numeros:
+        resultado = n1 * n2
+        #operacao= f"{n1} x {n2} = {n1 * n2}"
+        #print("{n1} x {n2} = {resultado}\n")
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print()
+    print("#" * 18)
